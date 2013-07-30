c78aabe2c992ad845873541a5380e6430e043c40
Tony Cheneau <tony.cheneau@nist.gov>
2013-07-30 10:31:21 -0400

    Add a new type of frame, for the event that should be registered by the event logger.
    The event logger does not exist yet, but in the future, the simulated
    nodes will be able to reach for such an entity on order to log special
    events (e.g. node joins simulation, node authenticates to its neighbor,
    etc.)

--- network/frame.hpp
+++ network/frame.hpp
@@ -10,7 +10,8 @@ class Frame {
 public:
 	typedef std::vector<uint8_t> frame;
 	enum type { INBOUND_FRAME, SIM_STOP, /* from the clients */
-			  OUTBOUND_FRAME, SIM_END /* from the server */ };
+			  OUTBOUND_FRAME, SIM_END, /* from the server */
+			  MISC_DATA = 128 /* type for events registered by the simulation logger */ };
 	static frame build_outbound_frame(const Node<>::node_ptr sender,
 									  const Node<>::node_list good,
 									  const Node<>::node_list bad,
